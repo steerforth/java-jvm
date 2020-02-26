1fa0126396f9d472293c3a16c52af37ec153258e
steer <565355716@qq.com>
2020-02-26 22:15:45 +0800

    自定义类加载器

---
+++ jvm/src/com/steer/jvm/classloader/CustomClassLoader.java
@@ -0,0 +1,24 @@
+package com.steer.jvm.classloader;
+
+import java.io.FileNotFoundException;
+
+public class CustomClassLoader extends ClassLoader {
+    @Override
+    protected Class<?> findClass(String name) throws ClassNotFoundException {
+        try {
+            byte[] result = getClassFromCustomPath(name);
+            if (result == null){
+                throw new FileNotFoundException();
+            }else {
+                return defineClass(name, result,0, result.length);
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return super.findClass(name);
+    }
+
+    private byte[] getClassFromCustomPath(String name) {
+        return null;
+    }
+}
